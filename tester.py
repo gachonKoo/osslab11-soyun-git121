fe94e9f1147de1eca5fd1c22ee8e250efe26d5a4
soyun-git121 <soyunbag066@gmail.com>
2024-11-17 20:15:50 +0900

    Add files via upload

--- tester.py
+++ tester.py
@@ -1,9 +1,9 @@
 import geo.utils as utils
 
-a, b = 3, 4
-c = utils.hypot(a, b) 
-print("c =", c)
+a,b = 3,4
+c = utils.pythagoras(a, b)
+print ("c =", c)
 
 r = 10
-area = utils.circle_area(r) 
+area = utils.circle(r)
 print("area =", area)
